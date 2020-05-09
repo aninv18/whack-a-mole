34914669c5f8f9516f9d5d65e02f92fd1176a957
aninv18 <37840256+aninv18@users.noreply.github.com>
2020-05-09 19:32:43 +0530

    game logic and design

---
+++ Assets/mole.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mole : MonoBehaviour
+
+{
+
+
+    Vector3 nextpos;
+    public float top = 4.8f;
+    public float bottom = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {        
+        nextpos = new Vector3(transform.localPosition.x, top, transform.localPosition.z);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {    
+        if (transform.position.y == top)
+            nextpos.y = bottom;
+
+        if (transform.position.y == bottom)
+            nextpos.y = top;
+
+        transform.position = Vector3.MoveTowards(transform.position, nextpos, Time.deltaTime * 4);
+
+    }
+
+    public void Disp()
+    {
+        Debug.Log(transform.name);
+    }
+}
+
+
---
+++ Assets/randomizer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomizer : MonoBehaviour
+
+{
+    public GameObject container;
+    mole [] m;
+    private int index;
+    private bool x;
+    float x1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = container.GetComponentsInChildren<mole>();      
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x1 += Time.deltaTime;
+        
+        if(x1 >= 0.9f)
+        {
+            x = false;
+            if ( m[index].transform.position.y <= (m[index].bottom+0.1f))
+            {
+                m[index].enabled = false;                
+                x = true;
+            }
+
+            if (x)
+            {
+                index = Random.Range(0, m.Length);
+                m[index].enabled = true;
+                x1 = 0;
+            }
+            
+
+       }
+        
+        
+            
+           
+
+
+
+
+
+
+
+    }
+    public void next()
+    {
+       
+
+        
+        
+
+    }
+    
+}
